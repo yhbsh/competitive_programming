ae585df78b6d25ef133810f8f032c4e98c6297f7
HoussemBousmaha <ky_bousmaha@esi.dz>
2022-02-15 06:34:03 +0300

    Create palindrome_number.py

---
+++ palindrome_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        for i in range(len(x) // 2):
+            if x[i] != x[len(x) - i - 1]:
+                return False
+            
+        return True
