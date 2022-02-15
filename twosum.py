210bdfc5faa24ab71dbcc70ab6560fc3333bd646
HoussemBousmaha <ky_bousmaha@esi.dz>
2022-02-15 06:33:11 +0300

    Create twosum.py

---
+++ twosum.py
@@ -0,0 +1,5 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if (target - nums[i]) in nums and i != nums.index(target-nums[i]):
+                return [i, nums.index(target-nums[i])]
