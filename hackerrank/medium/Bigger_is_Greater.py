2d0419199e8abf3e7bfd6461b3368a4a74c1395c
Youcef Houssem Eddine Bousmaha <ky_bousmaha@esi.dz>
2022-03-31 07:58:19 +0300

    Hackerrank hard problem "Bigger is Greater" python solution

---
+++ hackerrank/medium/Bigger_is_Greater.py
@@ -0,0 +1,22 @@
+def biggerIsGreater(w):
+    arr = list(w)
+    l = ""
+    # Find non-increasing suffix
+    i = len(arr) - 1
+    while i > 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    if i <= 0:
+        return "no answer"
+
+    # Find successor to pivot
+    j = len(arr) - 1
+    while arr[j] <= arr[i - 1]:
+        j -= 1
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+
+    # Reverse suffix
+    arr[i:] = arr[len(arr) - 1: i - 1: -1]
+
+    for i in arr:
+        l += i
+    return l
